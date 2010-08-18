ffbed32497da95c741a903ed8c0bec0dcf539883
Théodore Biadala <mail@theodoreb.net>
2010-08-18 09:39:50 +0200

    Clean up the code, now shorter :)

--- nod.js
+++ nod.js
@@ -4,29 +4,15 @@ b=document.body,
 c=b.children[0],
 a=c.getContext("2d"),
 p=2*Math.PI,
-ww=w.innerWidth,
-wh=w.innerHeight,
-interval=800,
-red='e00',
-green='0e0',
-queue = (function (q,pos) {
-  q=[],pos=0;
-  return {
-    push: function (f,p) {
-      q.push(function() {f(p);});
-      q.push(w.onresize);
-    },
-    process: function() {
-      q.forEach(function (e, i) {to(e,i*interval);});
-      to(restart,q.length*interval+3*interval);
-    },
-    clear:function () {
-      q=[];
-    }
-  };
-}());
+ww,
+wh,
+n=800;
 b.style.margin="0";
 c.style.display="block";
+function push(f,p) {
+  q.push(function() {f(p);});
+  q.push(w.onresize);
+}
 function to(f,i) {setTimeout(f,i);}
 function drawDot(color){
   a.fillStyle='#'+color;
@@ -35,17 +21,19 @@ function drawDot(color){
   a.closePath();
   a.fill();
 }
-function restart() {w.onresize();queue.clear();clock();}
-(w.onresize=function(){
+w.onresize=function(){
   ww=c.width=w.innerWidth;
   wh=c.height=w.innerHeight;
   a.fillRect(0,0,c.width,c.height);
-})();
+};
 (clock=function(h,m,t){
+  w.onresize();
+  q=[];
   t=new Date;
   h=t.getHours()%12;
   m=~~(t.getMinutes()/5);
-  while(h--){queue.push(drawDot,red);}
-  while(m--){queue.push(drawDot,green);}
-  queue.process();
+  while(h--){push(drawDot,'e00');}
+  while(m--){push(drawDot,'0e0');}
+  q.forEach(function (e,i) {to(e,i*n);});
+  to(clock,q.length*n+3*n);
 })();
